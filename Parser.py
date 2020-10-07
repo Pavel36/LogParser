6b6e9654ec4615d5131349bdfe36b2f1b43c3725
Pavel Gotsev <pgocev@list.ru>
2020-10-07 16:14:59 +0300

    Out

--- Parser.py
+++ Parser.py
@@ -5,27 +5,29 @@ import re
 
 def reader(filename, pattern):
     if os.path.exists(filename):
+        out = open(filename + '\\out.txt', 'w')
         if os.path.isfile(filename):
             if os.path.splitext(filename)[1] == '.log':
                 f = open(filename, 'r')
                 for line in f:
                     if re.search(pattern, line):
-                        print(line)
+                        out.write(line)
                 f.close()
         elif os.path.isdir(filename):
             for file in os.listdir(filename):
                 if os.path.splitext(file)[1] == '.log':
-                    print('filename=', file)
+                    out.write('filename: ' + file + '\n')
                     f = open(filename + '\\' + file, 'r')
                     for line in f:
                         if re.search(pattern, line):
-                            print(line)
+                            out.write(line)
                     f.close()
+        out.close()
     else:
         print('file not found')
 
 
-#reader(sys.argv[1], sys.argv[2])
+# reader(sys.argv[1], sys.argv[2])
 # py Parser.py D:\Logs Error:
 
 reader("D:\Logs", "VERBOSE")
