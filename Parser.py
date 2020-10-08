04da1eb5d91248836db7ae81e7bc1d7f3dcf6cb5
Pavel Gotsev <pgocev@list.ru>
2020-10-08 14:20:39 +0300

    Comments fixes

--- Parser.py
+++ Parser.py
@@ -3,31 +3,36 @@ import os
 import re
 from tqdm import tqdm
 
-def reader(filename, pattern):
+
+def reader(filename, pattern, allow_ext):
     if os.path.exists(filename):
-        out = open(filename + '\\out.txt', 'w')
-        if os.path.isfile(filename):
-            if os.path.splitext(filename)[1] == '.log':
-                f = open(filename, 'r')
-                for line in f:
-                    if re.search(pattern, line):
-                        out.write(line)
-                f.close()
-        elif os.path.isdir(filename):
-            for file in tqdm(os.listdir(filename)):
-                if os.path.splitext(file)[1] == '.log':
-                    out.write('filename: ' + file + '\n')
-                    f = open(filename + '\\' + file, 'r')
-                    for line in f:
+        with open(filename + '\\out.txt', 'w') as out:
+            if os.path.isfile(filename):
+                if re.search(allow_ext, os.path.splitext(filename)[1]):
+                    f = open(filename, 'r')
+                    for line in tqdm(f.readlines(), desc=filename):
                         if re.search(pattern, line):
                             out.write(line)
                     f.close()
+            elif os.path.isdir(filename):
+                for file in os.listdir(filename):
+                    if os.path.splitext(file)[1] == '.log':
+                        out.write('filename: ' + file + '\n')
+                        f = open(filename + '\\' + file, 'r')
+                        for line in tqdm(f.readlines(), desc=file):
+                            if re.search(pattern, line):
+                                out.write(line)
+                        f.close()
         out.close()
     else:
         print('file not found')
 
 
-reader(sys.argv[1], sys.argv[2])
-# py Parser.py D:\Logs Error:
+# reader("D:\Logs", "VERBOSE", '.log')
 
-#reader("D:\Logs", "VERBOSE")
+if len(sys.argv) == 4:
+    reader(sys.argv[1], sys.argv[2], sys.argv[3])
+else:
+    print('wrong number of arguments')
+
+# py Parser.py D:\Logs Error: .log
